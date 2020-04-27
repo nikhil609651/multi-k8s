7c1311ab86484235624da5b5c7576e186ec4edf2
Nikhil R <rnikhil@GMG.LOCAL>
2020-04-27 18:23:25 +0530

    deployses

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,12 @@
 docker build -t nikhil609651/multi-client:latest -t nikhil609651/multi-client:$SHA -f ./client/Dockerfile ./client
 docker build -t nikhil609651/multi-server:latest -t nikhil609651/multi-server:$SHA -f ./server/Dockerfile ./server
 docker build -t nikhil609651/multi-worker:latest -t nikhil609651/multi-worker:$SHA -f ./worker/Dockerfile ./worker
-docker push -t nikhil609651/multi-client:latest
-docker push -t nikhil609651/multi-server:latest
-docker push -t nikhil609651/multi-worker:latest
-docker push -t nikhil609651/multi-client:$SHA
-docker push -t nikhil609651/multi-server:$SHA
-docker push -t nikhil609651/multi-worker:$SHA
+docker push  nikhil609651/multi-client:latest
+docker push  nikhil609651/multi-server:latest
+docker push  nikhil609651/multi-worker:latest
+docker push  nikhil609651/multi-client:$SHA
+docker push  nikhil609651/multi-server:$SHA
+docker push  nikhil609651/multi-worker:$SHA
 kubectl apply -f k8s
 kubectl set image deployments/server-deployment server=nikhil609651/multi-server:$SHA
 kubectl set image deployments/client-deployment client=nikhil609651/multi-client:$SHA
